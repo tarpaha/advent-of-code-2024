0ca8f5991e5ad3184c63e7ffb6a569dc4c3584e2
tarpaha <ppopchikovsky@gmail.com>
2024-12-19 11:51:12 +0100

    day-2024-12-02 part1

--- day-2024-12-02.tests/SolverTests.cs
+++ day-2024-12-02.tests/SolverTests.cs
@@ -2,17 +2,36 @@ namespace day_2024_12_02.tests;
 
 public class SolverTests
 {
-    private const string Data = @"";
+    private const string Data = """
+                                7 6 4 2 1
+                                1 2 7 8 9
+                                9 7 6 2 1
+                                1 3 2 4 5
+                                8 6 4 4 1
+                                1 3 6 7 9
+                                """;
 
     [Test]
     public void Part1()
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(2));
     }
     
     [Test]
     public void Part2()
     {
         Assert.That(Solver.Part2(Parser.Parse(Data)), Is.Null);
+    }
+
+    [TestCase(0, true)]
+    [TestCase(1, false)]
+    [TestCase(2, false)]
+    [TestCase(3, false)]
+    [TestCase(4, false)]
+    [TestCase(5, true)]
+    public void IsReportSafe_Works_Correctly(int reportIndex, bool expected)
+    {
+        var data = Parser.Parse(Data);
+        Assert.That(Solver.IsReportSafe(data.Reports[reportIndex]), Is.EqualTo(expected));
     }
 }
--- day-2024-12-02/Solver.cs
+++ day-2024-12-02/Solver.cs
@@ -4,11 +4,29 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        return null!;
+        return data
+            .Reports
+            .AsParallel()
+            .Count(IsReportSafe);
     }
 
     public static object Part2(Data data)
     {
         return null!;
+    }
+
+    public static bool IsReportSafe(Report report)
+    {
+        var increasing = false;
+        var decreasing = false;
+        for (var i = 1; i < report.Levels.Length; i++)
+        {
+            var diff = report.Levels[i] - report.Levels[i - 1];
+            increasing |= diff > 0;
+            decreasing |= diff < 0;
+            if(diff == 0 || Math.Abs(diff) > 3 || (increasing && decreasing))
+                return false;
+        }
+        return true;
     }
 }
