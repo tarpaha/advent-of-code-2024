ec8419554aac3954ad71d06169b6339b601c2efd
tarpaha <ppopchikovsky@gmail.com>
2024-12-19 10:43:38 +0100

    day-2024-12-02 start

---
+++ day-2024-12-02.tests/SolverTests.cs
@@ -0,0 +1,18 @@
+namespace day_2024_12_02.tests;
+
+public class SolverTests
+{
+    private const string Data = @"";
+
+    [Test]
+    public void Part1()
+    {
+        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        Assert.That(Solver.Part2(Parser.Parse(Data)), Is.Null);
+    }
+}
