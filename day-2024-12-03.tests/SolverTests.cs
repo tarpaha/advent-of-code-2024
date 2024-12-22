12f60ef89b622d142a8cea7f4414a76dc1f0d986
tarpaha <ppopchikovsky@gmail.com>
2024-12-23 00:24:13 +0100

    day-2024-12-03 part2

--- day-2024-12-03.tests/SolverTests.cs
+++ day-2024-12-03.tests/SolverTests.cs
@@ -2,17 +2,15 @@ namespace day_2024_12_03.tests;
 
 public class SolverTests
 {
-    private const string Data = @"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-
-    [Test]
-    public void Part1()
+    [TestCase("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", 161)]
+    public void Part1(string input, int expected)
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(161));
+        Assert.That(Solver.Part1(Parser.Parse(input)), Is.EqualTo(expected));
     }
     
-    [Test]
-    public void Part2()
+    [TestCase("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", 48)]
+    public void Part2(string input, int expected)
     {
-        Assert.That(Solver.Part2(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part2(Parser.Parse(input)), Is.EqualTo(expected));
     }
 }
--- day-2024-12-03/Solver.cs
+++ day-2024-12-03/Solver.cs
@@ -4,89 +4,153 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        return GetMulInstructions(data.Memory)
+        return GetMulInstructions(data.Memory, false)
             .Select(numbers => numbers.firstNumber * numbers.secondNumber)
             .Sum();
     }
 
     public static object Part2(Data data)
     {
-        return null!;
+        return GetMulInstructions(data.Memory, true)
+            .Select(numbers => numbers.firstNumber * numbers.secondNumber)
+            .Sum();
     }
 
-    private static IEnumerable<(int firstNumber, int secondNumber)> GetMulInstructions(string memory)
+    private static IEnumerable<(int firstNumber, int secondNumber)> GetMulInstructions(string memory, bool useEnabled)
     {
-        var state = State.Start;
+        var mulState = MulState.Start;
         var firstNumber = 0;
         var secondNumber = 0;
+
+        var enablerState = EnablerState.Start;
+        var enableValueToSet = false;
+        var enabled = true;
+        
         foreach (var ch in memory)
         {
-            switch (state)
+            switch (enablerState)
             {
-                case State.Start:
-                    state = ch == 'm' ? State.M : State.Start;
+                case EnablerState.Start:
+                    enablerState = ch == 'd' ? EnablerState.D : EnablerState.Start;
                     break;
-                case State.M:
-                    state = ch == 'u' ? State.U : State.Start;
+                case EnablerState.D:
+                    enablerState = ch == 'o' ? EnablerState.O : EnablerState.Start;
                     break;
-                case State.U:
-                    state = ch == 'l' ? State.L : State.Start;
+                case EnablerState.O:
+                    switch (ch)
+                    {
+                        case '(':
+                            enablerState = EnablerState.Open;
+                            enableValueToSet = true;
+                            break;
+                        case 'n':
+                            enablerState = EnablerState.N;
+                            break;
+                        default:
+                            enablerState = EnablerState.Start;
+                            break;
+                    }
                     break;
-                case State.L:
-                    state = ch == '(' ? State.Open : State.Start;
+                case EnablerState.Open:
+                    enablerState = ch == ')' ? EnablerState.Close : EnablerState.Start;
                     break;
-                case State.Open:
+                case EnablerState.N:
+                    enablerState = ch == '\'' ? EnablerState.Apostrophe : EnablerState.Start;
+                    break;
+                case EnablerState.Apostrophe:
+                    enablerState = ch == 't' ? EnablerState.T : EnablerState.Start;
+                    break;
+                case EnablerState.T:
+                    if (ch == '(')
+                    {
+                        enablerState = EnablerState.Open;
+                        enableValueToSet = false;
+                    }
+                    else
+                        enablerState = EnablerState.Start;
+                    break;
+            }
+
+            if (enablerState == EnablerState.Close)
+            {
+                enabled = enableValueToSet;
+                enablerState = EnablerState.Start;
+            }
+
+            switch (mulState)
+            {
+                case MulState.Start:
+                    mulState = ch == 'm' ? MulState.M : MulState.Start;
+                    break;
+                case MulState.M:
+                    mulState = ch == 'u' ? MulState.U : MulState.Start;
+                    break;
+                case MulState.U:
+                    mulState = ch == 'l' ? MulState.L : MulState.Start;
+                    break;
+                case MulState.L:
+                    mulState = ch == '(' ? MulState.Open : MulState.Start;
+                    break;
+                case MulState.Open:
                     if (char.IsDigit(ch))
                     {
-                        state = State.FirstNumber;
+                        mulState = MulState.FirstNumber;
                         firstNumber = ch - '0';
                     }
                     else
-                        state = State.Start;
+                        mulState = MulState.Start;
                     break;
-                case State.FirstNumber:
+                case MulState.FirstNumber:
                     if (char.IsDigit(ch))
                         firstNumber = firstNumber * 10 + ch - '0';
                     else if(ch == ',')
-                        state = State.Comma;
+                        mulState = MulState.Comma;
                     else
-                        state = State.Start;
+                        mulState = MulState.Start;
                     break;
-                case State.Comma:
+                case MulState.Comma:
                     if (char.IsDigit(ch))
                     {
-                        state = State.SecondNumber;
+                        mulState = MulState.SecondNumber;
                         secondNumber = ch - '0';
                     }
                     else
-                        state = State.Start;
+                        mulState = MulState.Start;
                     break;
-                case State.SecondNumber:
+                case MulState.SecondNumber:
                     if (char.IsDigit(ch))
                         secondNumber = secondNumber * 10 + ch - '0';
                     else if (ch == ')')
                     {
-                        state = State.Close;
+                        mulState = MulState.Close;
                     }
                     else
-                        state = State.Start;
+                        mulState = MulState.Start;
                     break;
             }
 
-            if (state == State.Close)
+            if (mulState == MulState.Close)
             {
-                yield return (firstNumber, secondNumber);
-                state = State.Start;
+                if(!useEnabled || enabled)
+                    yield return (firstNumber, secondNumber);
+                mulState = MulState.Start;
             }
         }
     }
 
-    private enum State
+    private enum MulState
     {
         Start,
         M, U, L,
         Open,
         FirstNumber, Comma, SecondNumber,
         Close
+    }
+
+    private enum EnablerState
+    {
+        Start,
+        D, O, Open, Close,
+        N, Apostrophe, T,
     }
 }
