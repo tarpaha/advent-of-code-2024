8c7333fa5ab9e91db006a930c0251888d0e90925
tarpaha <ppopchikovsky@gmail.com>
2024-12-25 17:12:20 +0100

    day-2024-12-05 part1

--- day-2024-12-05.tests/SolverTests.cs
+++ day-2024-12-05.tests/SolverTests.cs
@@ -2,12 +2,41 @@ namespace day_2024_12_05.tests;
 
 public class SolverTests
 {
-    private const string Data = @"";
+    private const string Data = """
+                                47|53
+                                97|13
+                                97|61
+                                97|47
+                                75|29
+                                61|13
+                                75|53
+                                29|13
+                                97|29
+                                53|29
+                                61|53
+                                97|53
+                                61|29
+                                47|13
+                                75|47
+                                97|75
+                                47|61
+                                75|61
+                                47|29
+                                75|13
+                                53|13
+
+                                75,47,61,53,29
+                                97,61,53,29,13
+                                75,29,13
+                                75,97,47,61,53
+                                61,13,29
+                                97,13,75,29,47
+                                """;
 
     [Test]
     public void Part1()
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(143));
     }
     
     [Test]
--- day-2024-12-05/Solver.cs
+++ day-2024-12-05/Solver.cs
@@ -4,11 +4,30 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        return null!;
+        var deniedCombinations = data.Rules
+            .Select(rule => (rule.Right, rule.Left))
+            .ToHashSet();
+
+        return data.Updates
+            .Where(update => UpdateIsCorrect(update, deniedCombinations))
+            .Sum(update => update.Numbers[update.Numbers.Count / 2]);
     }
 
     public static object Part2(Data data)
     {
         return null!;
+    }
+    
+    private static bool UpdateIsCorrect(Update update, HashSet<(int, int)> deniedCombinations)
+    {
+        for (var left = 0; left < update.Numbers.Count; left++)
+        {
+            for (var right = left + 1; right < update.Numbers.Count; right++)
+            {
+                if (deniedCombinations.Contains((update.Numbers[left], update.Numbers[right])))
+                    return false;
+            }
+        }
+        return true;
     }
 }
